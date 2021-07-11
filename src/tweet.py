fc13036cc20c74aa388975d0ec8315c2f648958f
takagi <iam.takagi.dev@gmail.com>
2021-07-11 20:18:26 +0900

    Update tweet.py

--- src/tweet.py
+++ src/tweet.py
@@ -4,14 +4,10 @@ import numpy as np
 from twitterApi import api 
 
 def tweet():
-    exportTweets()
-
     # 10%の確率で「にゃんぱすー」を呟く
     if np.random.randint(1,91) == 1:
         api.update_status(status = "にゃんぱすー")
-        sentence_1, sentence_2 = make_sentences()
-        api.update_status(status = sentence_1)
-        api.update_status(status = sentence_2)
+    sentence_1, sentence_2 = make_sentences()
+    api.update_status(status = sentence_1)
+    api.update_status(status = sentence_2)
 
-if __name__ == '__main__':
-    tweet()
