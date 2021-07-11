7d43e42918592568beee6dfcca1811ff80024013
takagi <iam.takagi.dev@gmail.com>
2021-07-11 19:00:30 +0900

    Update tweet.py

--- src/tweet.py
+++ src/tweet.py
@@ -1,13 +1,18 @@
+import tweepy
 from makeSentences import make_sentences
 import numpy as np
 from twitterApi import api 
 from exportTweets import exportTweets
 
-exportTweets()
+def tweet():
+    exportTweets()
 
-# 10%の確率で「にゃんぱすー」を呟く
-if np.random.randint(1,91) == 1:
-    api.update_status(status = "にゃんぱすー")
-    sentence_1, sentence_2 = make_sentences()
-    api.update_status(status = sentence_1)
-    api.update_status(status = sentence_2)
+    # 10%の確率で「にゃんぱすー」を呟く
+    if np.random.randint(1,91) == 1:
+        api.update_status(status = "にゃんぱすー")
+        sentence_1, sentence_2 = make_sentences()
+        api.update_status(status = sentence_1)
+        api.update_status(status = sentence_2)
+
+if __name__ == '__main__':
+    tweet()
