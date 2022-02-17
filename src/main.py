433e4c84b8f0ad161a1c89c4a69139538af8bfcb
takagi <iam.takagi.dev@gmail.com>
2022-02-17 18:00:54 +0900

    Feat: Auto Reply

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from tweet import tweet
 import logging
+from replyStream import ReplyStreamListener, ReplyStream
+from twitterAuth import auth
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -13,4 +15,7 @@ def cron_tweet():
     tweet()
     
 if __name__ == "__main__":
+    listener = ReplyStreamListener()
+    stream = ReplyStream(auth, listener)
+    stream.start()
     sched.start()
