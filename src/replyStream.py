28e5d48aaa36197c33be4114656313c97deaed44
takagi <iam.takagi.dev@gmail.com>
2022-02-17 12:12:07 +0900

    Implements

---
+++ src/replyStream.py
@@ -0,0 +1,32 @@
+from tweepy import Stream, StreamListener
+from makeReplySentence import makeReplySentence
+from twitterApi import api
+
+# Twitter streaming
+class ReplyStreamListener(StreamListener):
+
+    def on_status(self, status):
+        print("[Info] Retrieved tweet: ", status.text)
+        reply_msg = makeReplySentence(status)
+        if "@nyanpassnanon" in reply_msg:
+            pass
+            print("This tweet contains reply to @nyanpassnanon, skipped.")
+        else:
+            api.update_status(reply_msg, in_reply_to_status_id=status.id)
+            print("Sent tweet: {}".format(reply_msg))
+        return True
+
+    def on_error(self, status_code):
+        if status_code == 420:
+            print('[Error] 420')
+            return False
+        else:
+            print(f'[Error] {status_code}')
+            return False
+
+class ReplyStream():
+    def __init__(self, auth, listener):
+        self.stream = Stream(auth=auth, listener=listener)
+
+    def start(self):
+        self.stream.filter(track=["@nyanpassnanon"])
