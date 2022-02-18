a1cc5d70575740f2915c05b7fd4895babbc2789a
takagi <iam.takagi.dev@gmail.com>
2022-02-18 18:37:03 +0900

    Fix: json.dumps

--- src/timelineTweets.py
+++ src/timelineTweets.py
@@ -1,22 +1,23 @@
 import os
 
 import json
+
 from twitterApi import api
 
 dest = 'data/timline_tweets.json'
 
 def fetch_timeline_tweets():
     tweets = [s.text for s in api.home_timeline(count = 100) if not s.user.screen_name == os.environ["SCREEN_NAME"] and not s.retweeted and 'RT @' not in s.text]
-    if os.path.isfile("data/home_timeline.json"):
+    if os.path.isfile(dest):
         with open(dest, mode = 'r+') as current:
             current.truncate(0)
             current.close()
     with open(dest, mode = "w") as file:
-        file.write(tweets)
+        file.write(json.dumps(tweets))
 
 def get_tweets():
     tweets = []
-    if not os.path.isfile("data/home_timeline.json"):
+    if not os.path.isfile(dest):
         return tweets
     with open(dest, "r") as file:
         json = json.load(file)
