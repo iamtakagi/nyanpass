f7ac9cc90a78c341f70bb5a4e94ed21e9ccf1172
takagi <iam.takagi.dev@gmail.com>
2022-02-18 13:58:45 +0900

    多分動く

--- src/main.py
+++ src/main.py
@@ -13,9 +13,12 @@ class Config(object):
 @sched.scheduled_job('cron', id='tweet', minute='*/15')
 def cron_tweet():
     tweet()
-    
+
+@sched.scheduled_job('interval', id='reply_stream' seconds=15)　
+def reply_stream():
+　　 listener = ReplyStreamListener()
+    stream = ReplyStream(auth, listener)
+    stream.start()
+
 if __name__ == "__main__":
-    #listener = ReplyStreamListener()
-    #stream = ReplyStream(auth, listener)
-    #stream.start()
     sched.start()
