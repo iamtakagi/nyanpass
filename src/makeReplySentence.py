87940ad1ada0165dd774c189a14ffc599e0d6920
takagi <iam.takagi.dev@gmail.com>
2022-02-18 20:29:39 +0900

    Improve: check regex

--- src/makeReplySentence.py
+++ src/makeReplySentence.py
@@ -5,6 +5,7 @@ import numpy as np
 from makeTweetSentences import make_tweet_sentences
 from timelineTweets import get_tweets, fetch_timeline_tweets
 from filters import normalize_text
+import re
 
 # MeCab
 mecab = MeCab.Tagger(f"-d /usr/lib/mecab/dic/mecab-ipadic-neologd -Ochasen")
@@ -14,10 +15,10 @@ def make_reply_sentence(status):
     text = normalize_text(status.text)
     text = text.replace(",", "").replace("&lt;", "<").replace("&gt;", ">").replace("&amp;", "&").replace("?", "？").replace("!", "！").replace("，", "、").replace("．", "。").replace('@nyanpassnanon', "")
     # 占い
-    if "占って" in text or "うらなって" in text or "おみくじ" in text:
+    if re.compile(r"(?:(?:うらな|占)って|おみくじ)").search(text):
         return "@{} {}なん！".format(screen_name, random.choice(("凶", "大凶", "末吉", "吉", "小吉", "中吉", "大吉")))
     # じゃんけん
-    if "グー" in text or "チョキ" in text or "パー" in text or "ぐー" in text or "ちょき" in text or "パー" in text:
+    if re.compile(r"(?:[ぐぱグパ]ー|ちょき|チョキ)").search(text):
         result = random.choice(("グー", "チョキ", "パー"))
         text = text.replace("ぐー", "グー").replace("ちょき", "チョキ").replace("ぱー", "パー")
          # あいこ
