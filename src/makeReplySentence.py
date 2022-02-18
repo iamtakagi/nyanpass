4c1eed6283d7367b01326deb96c62f133f79285c
takagi <iam.takagi.dev@gmail.com>
2022-02-18 21:37:01 +0900

    Improve: make_reply_sentence

--- src/makeReplySentence.py
+++ src/makeReplySentence.py
@@ -10,28 +10,34 @@ import re
 # MeCab
 mecab = MeCab.Tagger(f"-d /usr/lib/mecab/dic/mecab-ipadic-neologd -Ochasen")
 
+def omikuji(screen_name):
+    return "@{} {}なん！".format(screen_name, random.choice(("凶", "大凶", "末吉", "吉", "小吉", "中吉", "大吉")))
+
+def janken(screen_name, text):
+    result = random.choice(("グー", "チョキ", "パー"))
+    text = text.replace("ぐー", "グー").replace("ちょき", "チョキ").replace("ぱー", "パー")
+    janken = ""
+    # あいこ
+    if result == text: 
+        janken = "@{} {}なん！あいこなん！".format(screen_name, result)
+    # 勝ちパターン
+    if result == "グー" and text == "チョキ" or result == "チョキ" and text == "パー" or result == "パー" and text == "グー":
+        janken = "@{} {}なん！うちの勝ちなん！".format(screen_name, result)
+    # 負けパターン
+    if result == "グー" and text == "パー" or result == "チョキ" and text == "グー" or result == "パー" and text == "チョキ":
+        janken = "@{} {}なん！うちの負けなん！".format(screen_name, result)
+    return janken
+
 def make_reply_sentence(status):
     screen_name = status.user.screen_name
     text = normalize_text(status.text)
     text = text.replace(",", "").replace("&lt;", "<").replace("&gt;", ">").replace("&amp;", "&").replace("?", "？").replace("!", "！").replace("，", "、").replace("．", "。").replace('@nyanpassnanon', "")
     # 占い
     if re.compile(r"(?:(?:うらな|占)って|おみくじ)").search(text):
-        return "@{} {}なん！".format(screen_name, random.choice(("凶", "大凶", "末吉", "吉", "小吉", "中吉", "大吉")))
+        return omikuji(screen_name)
     # じゃんけん
     if re.compile(r"(?:[ぐぱグパ]ー|ちょき|チョキ)").search(text):
-        result = random.choice(("グー", "チョキ", "パー"))
-        text = text.replace("ぐー", "グー").replace("ちょき", "チョキ").replace("ぱー", "パー")
-        janken = ""
-        # 勝ちパターン
-        if result == "グー" and text == "チョキ" or result == "チョキ" and text == "パー" or result == "パー" and text == "グー":
-            janken = "@{} {}なん！うちの勝ちなん！".format(screen_name, result)
-        # 負けパターン
-        if result == "グー" and text == "パー" or result == "チョキ" and text == "グー" or result == "パー" and text == "チョキ":
-            janken = "@{} {}なん！うちの負けなん！".format(screen_name, result)
-        # あいこ
-        if result == text: 
-            janken = "@{} {}なん！あいこなん！".format(screen_name, result)
-        return janken
+        return janken(screen_name, text)
     if text:
         if not get_tweets():
             fetch_timeline_tweets()
