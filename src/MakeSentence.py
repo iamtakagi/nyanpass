06fa01311e8d88ca5a03bd0fb014cd0c0ca28573
tsukumi <tsukumijima@users.noreply.github.com>
2022-05-09 04:20:42 +0900

    Update: ランダムに取得した名詞に記号しか含まれていなかったら再取得する

--- src/MakeSentence.py
+++ src/MakeSentence.py
@@ -1,9 +1,9 @@
 
 import json
 import numpy as np
-from typing import Tuple
 
 from ChoiceRandomNoun import ChoiceRandomNoun
+from TweetFilters import NormalizeTweetText
 
 
 # テンプレートを読み込み
@@ -22,8 +22,15 @@ def MakeSentence() -> str:
     # テンプレートからランダムにチョイス
     template = np.random.choice(templates)
 
+    # ランダムな名詞を取得
+    noun = ChoiceRandomNoun()
+
+    # 名詞に記号しか含まれていなかったら再取得
+    if NormalizeTweetText(noun) == '':
+        noun = ChoiceRandomNoun()
+
     # テンプレートとランダムな名詞を組み合わせて文章を生成
-    sentence = template['gokan'] + ChoiceRandomNoun() + template['gobi']
+    sentence = template['gokan'] + noun + template['gobi']
 
     # 文章を返す
     return sentence
