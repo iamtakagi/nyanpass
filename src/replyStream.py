f06fbc86fbe4a5df8700f97efbd535ed94b4671c
takagi <iam.takagi.dev@gmail.com>
2022-05-17 06:50:29 +0900

    feat: 相手のリプも Discord Webhook に投げてみる

--- src/replyStream.py
+++ src/replyStream.py
@@ -8,15 +8,17 @@ class ReplyStreamListener(StreamListener):
 
     def on_status(self, status):
         print("[Info] Retrieved tweet: ", status.text)
+        status_link = 'https://twitter.com/{}/status/{}'.format(status.user.screen_name, status.id)
+        post_discord_webhook(status_link)
         reply_msg = make_reply_sentence(status)
         if reply_msg == None: pass
         if "@nyanpassnanon" in reply_msg:
             pass
             print("This tweet contains reply to @nyanpassnanon, skipped.")
         else:
-            result = api.update_status('@{} {}'.format(status.user.screen_name, reply_msg), in_reply_to_status_id=status.id)
-            tweet_link = 'https://twitter.com/nyanpassnanon/status/{}'.format(result.id)
-            post_discord_webhook(tweet_link)
+            reply_result = api.update_status('@{} {}'.format(status.user.screen_name, reply_msg), in_reply_to_status_id=status.id)
+            reply_link = 'https://twitter.com/nyanpassnanon/status/{}'.format(reply_result.id)
+            post_discord_webhook(reply_link)
         return True
 
     def on_error(self, status_code):
