f25402a3a0d3a290fc04e75142c3763079061d58
takagi <iam.takagi.dev@gmail.com>
2022-10-17 16:08:18 +0900

    fix: どうかな？

--- main.py
+++ main.py
@@ -41,6 +41,10 @@ replyStream: ReplyStream or None
 
 def initReplyStream():
     global replyStream
+    # 切断
+    replyStream.disconnect()
+    replyStream = None
+    # 接続
     replyStream = ReplyStream(
         os.environ['TWITTER_CK'], os.environ['TWITTER_CS'],
         os.environ['TWITTER_AT'], os.environ['TWITTER_ATS'],
