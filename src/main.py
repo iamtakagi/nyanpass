7bcc7ddede939172a647dcd1025a498009ab498e
tsukumi <tsukumijima@users.noreply.github.com>
2023-05-23 08:38:04 +0000

    Update: Stream 系 API の廃止に伴い一部機能を削除

--- src/main.py
+++ src/main.py
@@ -3,7 +3,6 @@ import logging
 import os
 from apscheduler.schedulers.blocking import BlockingScheduler
 
-from ReplyStream import ReplyStream
 from Tweet import OnAirNotificationTweet, Tweet
 
 
@@ -27,13 +26,6 @@ def CronTVTokyoOnAirTweet():
 @scheduler.scheduled_job('cron', id='BSTVTokyoOnAirTweet', day_of_week='mon', hour=0, minute=30)
 def CronBSTVTokyoOnAirTweet():
     OnAirNotificationTweet('#bstvtokyo')
-
-# リプライを返すストリームをスレッドで開始
-stream = ReplyStream(
-    os.environ['TWITTER_CK'], os.environ['TWITTER_CS'],
-    os.environ['TWITTER_AT'], os.environ['TWITTER_ATS'],
-)
-stream.filter(track=[f'@{os.environ["SCREEN_NAME"]}'], threaded=True)
 
 # スケジューリングを開始
 # BlockingScheduler なのでここでブロッキングされる
